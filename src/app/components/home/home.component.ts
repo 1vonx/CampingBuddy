21356248715e0ed35114ec03b6c644373bcb4df7
Ivona Minkovska <94076658+1vonx@users.noreply.github.com>
2023-03-08 17:14:57 +0100

    initial commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+    text = "Offering a wide selection of camping experiences that are sure to provide the perfect getaway for you and your family. Whether you're looking for a tranquil escape in the woods, an action-packed adventure in the mountains, or a relaxing stay by the beach, we have something for everyone.";
 }
