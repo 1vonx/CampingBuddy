fffbed0d9bf19feed54252a2d6fccc9a076231a1
Ivona Minkovska <94076658+1vonx@users.noreply.github.com>
2023-03-09 20:42:51 +0100

    added popular section in home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camp } from 'src/app/models/camp';
+import { CampsService } from 'src/app/service/camps/camps.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
     text = "Offering a wide selection of camping experiences that are sure to provide the perfect getaway for you and your family. Whether you're looking for a tranquil escape in the woods, an action-packed adventure in the mountains, or a relaxing stay by the beach, we have something for everyone.";
+    mostPopular: Camp[];
+
+    constructor(private campService: CampsService) {
+      this.mostPopular = this.campService.getMostPopularCamps();
+    }
+
+
 }
--- src/app/service/camps/camps.service.ts
+++ src/app/service/camps/camps.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { CAMPS } from 'src/app/data/camps';
 import { Camp } from 'src/app/models/camp';
 
 @Injectable({
@@ -6,126 +7,24 @@ import { Camp } from 'src/app/models/camp';
 })
 export class CampsService {
 
-CAMPS : Camp[]= [
-    {
-        "id": 0,
-        "name": "Gradishte",
-        "type": "RV/Van Camping",
-        "spots": 30,
-        "longitude": "40.993957",
-        "latitude": "20.802233",
-        "address": "Gradishte 503",
-        "dailyPrice": "10$",
-        "categories": "beach,bars,party,canoeing,snorkeling",
-        "imgUrl": "https://live.staticflickr.com/3082/3155610349_afcfddcbbc_c.jpg",
-    },
-    {
-        "id": 1,
-        "name": "Free Camp",
-        "type": "Backpacking",
-        "spots": 15,
-        "longitude": "41.679489",
-        "latitude": "20.735416",
-        "address": "Kichinitsa 413,Ohrid",
-        "dailyPrice": "FREE",
-        "categories": "beach,fishing,biking,swimming",
-        "imgUrl": "https://www.telegraph.co.uk/content/dam/Travel/2018/July/Scotland-campingGettyImages-526564828.jpg",
-    },
-    {
-        "id": 2,
-        "name": "Camper 4 Night",
-        "type": "Backyard Camping",
-        "spots": 24,
-        "longitude": "41.992792",
-        "latitude": "21.437236",
-        "address": "Mirche Acev, Skopje",
-        "dailyPrice": "30$",
-        "categories": "photography,fun activities,movie night,games",
-        "imgUrl": "https://images.unsplash.com/photo-1587547131116-a0655a526190?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FtcGluZyUyMHRlbnR8ZW58MHx8MHx8&w=1000&q=80",
-    },
-    {
-        "id": 3,
-        "name": "Ljubanishta",
-        "type": "Car Camping",
-        "spots": 18,
-        "longitude": "40.924428",
-        "latitude": "20.774201",
-        "address": "Ljubanishta,Ohrid",
-        "dailyPrice": "5$",
-        "categories": "cooking,paddle boarding,kayaking,canoeing ",
-        "imgUrl": "https://live.staticflickr.com/65535/51277454149_a8b1b0b68f_b.jpg"
-    },
-    {
-        "id": 4,
-        "name": "Titov Vrv Camp",
-        "type": "Survival Camping",
-        "spots": 10,
-        "longitude": "42.119786",
-        "latitude": "20.938561",
-        "address": "Shar Planina",
-        "dailyPrice": "6$",
-        "categories": "hiking,exploring,wood whittling,",
-        "imgUrl": "https://images.unsplash.com/photo-1464547323744-4edd0cd0c746?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fGNhbXBpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-    },
-    {
-        "id": 5,
-        "name": "Big Lake",
-        "type": "Overlanding",
-        "spots": 14,
-        "longitude": "21.205536",
-        "latitude": "40.969075",
-        "address": "Golemo Ezero,Nizepole",
-        "dailyPrice": "20$",
-        "categories": "mountain biking,hiking,swimming,picnic,star watching,offroading",
-        "imgUrl": "https://live.staticflickr.com/4328/35458139403_be1346e336_b.jpg",
-    },
-    {
-        "id": 6,
-        "name": "Brnicki Waterfall",
-        "type": "Hammock Camping",
-        "spots": 7,
-        "longitude": "21.666846",
-        "latitude": "41.083223",
-        "address": "Crna Reka,Rapesh",
-        "dailyPrice": "16$",
-        "categories": "hiking,fishing,hunting,cooking",
-        "imgUrl": "https://macedoniasightseeing.mk/storage/2017/02/b10c21a731ce655ab93125566da61747.jpg"
-    },
-    {
-        "id": 7,
-        "name": "Shula Mina",
-        "type": "Backpacking",
-        "spots": 60,
-        "longitude": "21.237450",
-        "latitude": "41.374642",
-        "address": "Shula Mina,Krushevo",
-        "dailyPrice": "40$",
-        "categories": "fun activities,firewood foraging,campfire bonding,climbing",
-        "imgUrl": "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGNhbXBpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+camps : Camp[] = CAMPS;
+randomNums: number[];
 
-    },
-    {
-        "id": 8,
-        "name": "Youth Association",
-        "type": "Backpacking",
-        "spots": 60,
-        "longitude": "21.237450",
-        "latitude": "41.374642",
-        "address": "Korab Planina,Tanushe",
-        "dailyPrice": "Free$",
-        "categories": "fun activities,firewood foraging,campfire bonding,climbing",
-        "imgUrl": "https://www.buffaloriver.com/wp-content/uploads/2015/10/camping-kyles-landing-scaled.jpg",
-    },
-];
-
-  constructor() { }
+  constructor() { 
+    this.randomNums = [Math.floor(Math.random()*8), Math.floor(Math.random()*8), Math.floor(Math.random()*8)];
+  }
 
   getAllCamps(): Camp[] {
-    return this.CAMPS;
+    return this.camps;
   }
 
   getCampById(id: string) {
-    return this.CAMPS.find(c => c.id === Number(id));
+    return this.camps.find(c => c.id === Number(id));
+  }
+
+  getMostPopularCamps(){
+    console.log(this.randomNums);
+    return this.camps.filter(c => this.randomNums.includes(c.id));
   }
 
 }
